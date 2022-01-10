c9e82a4ff25d151ac4b633ad9a5cea79c37212d3
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-01-10 08:39:34 -0800

    commit tree e6f03239f348f0b4eba52444bd97c1e7f0056154

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+   student = Student.create([{ first_name: 'Ben' }, { last_name: 'Dover' }])
+   Student.create(first_name: student.first_name, last_name: student.last_name)
